79e46a36cb8b423825997f5f65d7f491cacbe338
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-07-30 16:33:23 +0430

    add basename to carts router

--- store/urls.py
+++ store/urls.py
@@ -8,7 +8,7 @@ router = routers.SimpleRouter()
 router.register('collections', views.CollectionViewSet)
 router.register('products', views.ProductViewSet)
 router.register('customers', views.CustomerViewSet, basename='customers')
-router.register('carts', views.CartViewSet)
+router.register('carts', views.CartViewSet, basename='carts')
 
 domains_router = routers.NestedSimpleRouter(router, 'carts', lookup='carts')
 
