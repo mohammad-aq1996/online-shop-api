52e51d99399e6e55430ebb8a6c70fd0354d3f69f
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-07-30 20:53:31 +0430

    add OrderViewSet

--- store/views.py
+++ store/views.py
@@ -80,4 +80,36 @@ class CartItemViewSet(ModelViewSet):
         return models.CartItem.objects.filter(cart_id=self.kwargs['carts_pk']).select_related('product')
 
 
+class OrderViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
 
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serializer = serializers.CreateOrderSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = serializers.OrderSerializer(order)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return serializers.UpdateOrderSerializer
+        return serializers.OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return models.Order.objects.all()
+
+        customer_id = models.Customer.objects.only(
+            'id').get(user_id=user.id)
+        return models.Order.objects.filter(customer_id=customer_id)
